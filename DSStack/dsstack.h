b276dc5c45efac4c7efe4b01cc993555b6f0bdff
L-Arch <levi44b@gmail.com>
2024-03-11 20:51:22 -0400

    created DSStack class

---
+++ DSStack/dsstack.h
@@ -0,0 +1,20 @@
+#ifndef DSSTACK_H
+#define DSSTACK_H
+
+class DSStack{
+public:
+    DSStack();
+
+    ~DSStack();
+private:
+};
+
+DSStack::DSStack(){
+
+}
+
+DSStack::~DSStack(){
+
+}
+
+#endif // DSSTACK_H
