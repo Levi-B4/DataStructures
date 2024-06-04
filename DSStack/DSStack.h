d0a666c04f0d98697aa9f9d9a4a0b5c6c1b91665
Levi Butler <levi44b@gmail.com>
2024-06-03 20:12:39 -0400

    created stack constructors

---
+++ DSStack/DSStack.h
@@ -0,0 +1,50 @@
+#ifndef DSSTACK_H
+#define DSSTACK_H
+
+template <class T>
+class DSStack{
+    public:
+        /**
+         * @brief DSStack - default constructor
+         * @param resizeIncrement - increment of size increase when at max capacity, 10 by default
+         */
+        DSStack(int resizeIncrement = 10);
+        /**
+         * @brief DSStack - copy constructor
+         * @param other - reference to target stack
+         */
+        DSStack(const DSStack<T>& other);
+
+
+
+
+
+    private:
+        T* data;
+        int capacity;
+        int numIndexes = 0;
+        int resizeIncrement;
+};
+
+template <class T>
+DSStack<T>::DSStack(int resizeIncrement){
+    this->resizeIncrement = resizeIncrement;
+    capacity = resizeIncrement;
+    data = new T[capacity];
+}
+
+template <class T>
+DSStack<T>::DSStack(const DSStack<T>& other){
+    capacity = other.capacity;
+    numIndexes = other.numIndexes;
+    resizeIncrement = other.resizeIncrement;
+
+    data = new T[capacity];
+
+    for(int i = 0; i < numIndexes; i++){
+        this->data[i] = other.data[i];
+    }
+}
+
+
+#endif // DSSTACK_H
