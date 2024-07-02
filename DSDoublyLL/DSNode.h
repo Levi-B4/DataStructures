14e6929923530827465b92761368329319257732
Levi Butler <levi44b@gmail.com>
2024-07-02 14:14:46 -0400

    created base of classes for DSNode and DSDoublyLL

---
+++ DSDoublyLL/DSNode.h
@@ -0,0 +1,38 @@
+#ifndef DSNODE_H
+#define DSNODE_H
+
+template <class T>
+class DSNode{
+    public:
+        /**
+         * @brief DSNode - default constructor
+         */
+        DSNode();
+
+        /**
+         * @brief ~DSNode - default destructor
+         */
+        ~DSNode();
+
+    private:
+
+
+};
+
+/**
+ * @brief DSNode - default constructor
+ */
+template <class T>
+DSNode<T>::DSNode(){
+
+}
+
+/**
+ * @brief ~DSNode - default destructor
+ */
+template <class T>
+DSNode<T>::~DSNode(){
+
+}
+
+#endif // DSNODE_H
