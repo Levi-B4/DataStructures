591b0bb338a88037d2a46834c0d35b57609f247a
Levi <levi.butler4@gmail.com>
2025-09-16 18:40:34 -0400

    Refactored adjacency list outline

--- DSAdjList/dsadjlist.h
+++ DSAdjList/dsadjlist.h
@@ -3,32 +3,11 @@
 
 #include "../DSDoublyLL/dsdoublyll.h"
 
-template <class TNode, class TWeight>
+template <class T>
 class DSAdjList
 {
 private:
-    /**
-     * @brief The NodeWeightPair class - holds a node and its connection weight
-     */
-    struct NodeWeightPair
-    {
-        NodeWeightPair(TNode node, TWeight weight): node(node), weight(weight){}
-        TNode node;
-        TWeight weight;
-    };
-
-    /**
-     * @brief The NodeEdges class - holds a node and all of its connections
-     */
-    struct NodeEdges
-    {
-        NodeEdges(TNode source, DSDoublyLL<NodeWeightPair> connections) : source(source), connections(connections){}
-
-        TNode source;
-        DSDoublyLL<NodeWeightPair> connections;
-    };
-
-    DSDoublyLL<NodeEdges> data;
+    DSDoublyLL<DSDoublyLL<T>> data;
 public:
     /**
      * @brief DSAdjList - default constructor
@@ -39,69 +18,103 @@ public:
      * @brief DSAdjList - copy constructor
      * @param other - list to copy
      */
-    DSAdjList(DSAdjList<TNode, TWeight>& other);
+    DSAdjList(DSAdjList<T>& other);
 
     /**
      * @brief GetConnectedNode - returns all nodes connected to the given nodes
-     * @param node - source node for connections
+     * @param nodeData - data of the source node for connections
      * @return doubly linked list of nodes
      */
-    DSDoublyLL<TNode> GetConnectedNodes(TNode node);
-
-    /**
-     * @brief GetEdgeWeight - returns the weight between two nodes
-     * @param n1 - first node of the connection
-     * @param n2 - second node of the connection
-     * @return the weight of the connected between the two given nodes
-     */
-    TWeight GetEdgeWeight(TNode n1, TNode n2);
-
-    /**
-     * @brief GetNodeOutEdges - returns all edges branching from a node
-     * @param node - source node for the returned edges
-     * @return a doubly linked list of a node and the edges weight
-     */
-    // TODO: look into returning an itterator instead
-    DSDoublyLL<NodeWeightPair> GetNodeOutEdges(TNode node);
-
-    /**
-     * @brief GetNodeInEdges - returns all edges branching to a node
-     * @param node - source node for the returned edges
-     * @return a doubly linked list a node and the edges weight
-     */
-    DSDoublyLL<NodeWeightPair> GetNodeInEdges(TNode node);
+    DSDoublyLL<T> GetConnectedNodes(T nodeData);
 
     /**
      * @brief addNode - adds a new node to the list
-     * @param node - the value of the new node
+     * @param nodeData - the value of the new node
      */
-    void addNode(TNode node);
+    void addNode(T nodeData);
 
     /**
      * @brief addEdge - adds a connection between two nodes with their weight
-     * @param source - the source node
-     * @param destination - the destination node
-     * @param weight - the weight associated with this edge
+     * @param node1 - first node of the edge
+     * @param node2 - second node of the edge
      */
-    void addEdge(TNode source, TNode destination, TWeight weight);
+    void addEdge(T node1, T node2);
 
     /**
      * @brief remove - removes a node and all of its edges
-     * @param element - the value of the node
+     * @param nodeData - the value of the node
      */
-    void removeNode(TNode element);
+    void removeNode(T nodeData);
 
     /**
      * @brief removeEdge - removes an edge from the list
-     * @param n1 - the source node
-     * @param n2 - the destination node
+     * @param node1 - first node of the edge
+     * @param node2 - second node of the edge
      */
-    void removeEdge(TNode source, TNode destination);
+    void removeEdge(T node1, T node2);
 
     /**
      * @brief ~DSAdList - default destructor
      */
     ~DSAdjList();
 };
+
+
+/**
+ * @brief DSAdjList - default constructor
+ */
+template <class T>
+DSAdjList<T>::DSAdjList(){}
+
+/**
+ * @brief DSAdjList - copy constructor
+ * @param other - list to copy
+ */
+template <class T>
+DSAdjList<T>::DSAdjList(DSAdjList<T>& other){}
+
+/**
+ * @brief GetConnectedNode - returns all linked list that is the tail of the given element's list
+ * @param nodeData - data of the source node for connections
+ * @return doubly linked list of nodes
+ */
+template <class T>
+DSDoublyLL<T> DSAdjList<T>::GetConnectedNodes(T nodeData){}
+
+/**
+ * @brief addNode - creates a new list at the end of the data list for the given node
+ * @param nodeData - the value of the new node
+ */
+template <class T>
+void DSAdjList<T>::addNode(T nodeData){}
+
+/**
+ * @brief addEdge - adds the given nodes to eachother's lists
+ * @param node1 - first node of the edge
+ * @param node2 - second node of the edge
+ */
+template <class T>
+void DSAdjList<T>::addEdge(T node1, T node2){}
+
+/**
+ * @brief remove - removes a node's list and all of its occurances in other lists
+ * @param nodeData - the value of the node
+ */
+template <class T>
+void DSAdjList<T>::removeNode(T nodeData){}
+
+/**
+ * @brief removeEdge - removes the given nodes from each others lists
+ * @param node1 - first node of the edge
+ * @param node2 - second node of the edge
+ */
+template <class T>
+void DSAdjList<T>::removeEdge(T node1, T node2){}
+
+/**
+ * @brief ~DSAdList - default destructor
+ */
+template <class T>
+DSAdjList<T>::~DSAdjList(){}
 
 #endif // DSADJLIST_H
